1cf851b83b7ed01c9acc62917a7494d5f5c283bb
Lorenzo Rinaldi <lorenzo.rinaldi13@gmail.com>
2024-11-02 08:27:28 +0100

    switched to mario new add sectors

--- add_sectors.py
+++ add_sectors.py
@@ -1,5 +1,5 @@
 #%%
-from fiona.core.db_builder import DB_builder
+import mario
 import yaml
 import os
 
@@ -14,23 +14,24 @@ git_folder = paths['git_folder'][user]
 master_file_path = os.path.join(git_folder,'support/add_sectors/master.xlsx')
 
 #%%
-db = DB_builder(
-    sut_path=os.path.join(onedrive_folder,paths['database']['exiobase']['aggregated'],'flows'),
-    sut_mode='flows',
-    master_file_path=master_file_path,
-    sut_format='txt',
-    read_master_file=True,
+db = mario.parse_from_txt(
+    path=os.path.join(onedrive_folder,paths['database']['exiobase']['aggregated'],'flows'),
+    mode='flows',
+    table='SUT',
 )
 
 #%%
-# db.read_master_template(master_file_path,get_inventories=True)
+# db.get_add_sectors_excel(master_file_path)
 
 #%%
-db.read_inventories(master_file_path,check_errors=False)
+db.read_add_sectors_excel(master_file_path,read_inventories=True)
 
 #%%
-db.add_inventories('excel')
+# db.read_inventory_sheets(master_file_path)
+
+#%%
+db.add_sectors()
 
 # %%
-db.sut.u.loc[:,('CN','Activity',db.new_activities)].sum(0)
+db.u.loc[:,('CN','Activity',db.new_activities)].sum(0)
 # %%
