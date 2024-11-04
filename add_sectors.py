1a6a7a8982f4666ac64ff0959d4605511e7fe9b3
Lorenzo Rinaldi <lorenzo.rinaldi13@gmail.com>
2024-11-04 14:11:44 +0100

    checked preliminary results

--- add_sectors.py
+++ add_sectors.py
@@ -33,5 +33,30 @@ db.read_add_sectors_excel(master_file_path,read_inventories=True)
 db.add_sectors()
 
 # %%
-db.u.loc[:,('CN','Activity',db.new_activities)].sum(0)
+ghgs = {
+    'Carbon dioxide, fossil (air - Emiss)':1,
+    'CH4 (air - Emiss)':26,
+    'N2O (air - Emiss)':298
+    }
+steel_acts = [
+    'Steel production through 100%H2-DR',
+    'Steel production with H2 inj to BF',
+    'Steel production with charcoal inj to BF',
+    'Steel production with charcoal inj to BF + CCUS',
+    'Steel production through NG-DR',
+    'Steel production BF-BOF + CCUS'
+    ]
+
+f = db.f.loc[ghgs.keys(),(slice(None),'Activity',steel_acts)]
+for ghg,gwp in ghgs.items():
+    f.loc[ghg,:] *= gwp
+f = f.sum(0)
+f = f.to_frame()    
+f.reset_index(inplace=True)
+f.columns = ['Region','Item','Activity','Value']
+f = f.drop('Item',axis=1)
+f.set_index(['Region','Activity'],inplace=True)
+f = f.unstack()
+f = f.droplevel(0,axis=1)
+f.to_clipboard()
 # %%
